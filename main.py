f9d3111fc61bf9c85494293c680e490c5f1af5a3
Ggoose <benmidds@outlook.com>
2025-08-15 04:57:13 +0100

    Added trails, fixed error in previous commit from main

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ clock = pygame.time.Clock()
 # Initialize display
 SCREEN_DIMENSIONS = (1280, 720)
 
-
 screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
 pygame.display.set_caption("Night sky")
 
@@ -19,11 +18,14 @@ pygame.display.set_caption("Night sky")
 background = pygame.surface.Surface(screen.get_rect().size)
 background.fill("black")
 
+fade_surf = pygame.Surface(screen.get_rect().size, pygame.SRCALPHA)
+fade_surf.fill((0x00, 0x00, 0x00, 0x08))
+
 # Initialize stars
 stars = pygame.sprite.Group()
 star1 = Star(screen, 100, 100, stars)
 for i in range (200):
-    new_star = Star(screen, random.randint(0, SCREEN_X), random.randint(0, SCREEN_Y), stars)
+    new_star = Star(screen, random.randint(0, screen.get_width()), random.randint(0, screen.get_height()), stars)
 
 
 # Simulation loop
@@ -42,11 +44,15 @@ while running:
 
     # Update graphics
     screen.blit(background, (0, 0))
-    background.set_at((int(star1.pos.x), int(star1.pos.y)), "GREEN")
 
     for star in stars:
         star.update_graphics(screen)
+        pygame.draw.line(background, "PURPLE", (int(star.pos.x), int(star.pos.y)), (int(star.pos.x), int(star.pos.y)), 3)
     
+    background.set_at((int(star1.pos.x), int(star1.pos.y)), "GREEN")
+
+    background.blit(fade_surf, (0, 0))
+
     pygame.display.flip()
 
     # Tick clock
